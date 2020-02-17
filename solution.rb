e06b3df6e85e42d9f9cc91cac7a7073dc36166f9
placidowang <placidow@gmail.com>
2020-02-17 14:24:25 -0600

    Done.

--- solution.rb
+++ solution.rb
@@ -1,1 +1,24 @@
+nums = [0, 1, 0, 3, 12]
+more_nums = [2, 7, 11, 15]
 
+def move_zeroes(nums)
+    zeroes = nums.count(0)
+    nums.delete(0)
+    zeroes.times {nums << 0}
+    nums
+end
+
+def two_sum(numbers, target)
+    h = {}
+    i = 0
+    while i < numbers.length do
+        complement = target - numbers[i]
+        if h.key?(complement)
+            return [h[complement], i]
+        end
+        h[numbers[i]] = i
+        i += 1
+    end
+end
+
+# p two_sum(more_nums, 22)
